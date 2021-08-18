46c8299e9fd0b0150dfde198dc1a290a26eabed7
nscando <nicolas.scandizzo@gmail.com>
2021-08-18 18:46:16 -0300

    Update MENU

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,11 @@
+import static ui.UIMenu.*;
+
 public class Main {
     public static void main(String[] args) {
-        Doctor myDoctor = new Doctor();
-        myDoctor.name = "Matias Scandizzo";
-        myDoctor.showName();
-        myDoctor.showId();
-        System.out.println(Doctor.id);
 
-        Doctor.id++;
-
-        Doctor myDoctorNico = new Doctor();
-        myDoctor.showId();
-        System.out.println(Doctor.id);
+        showMenu();
     }
+
+
+
 }
